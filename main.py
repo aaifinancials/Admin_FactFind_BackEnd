c7561bd7a8debc59058e874ca4b6b20673c35593
Gaurav N2 <gauravnehare1411@gmail.com>
2025-11-04 17:48:14 +0530

    main.py update

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import  user_auth, referrals, admin, user_details, mortgage_applications, anaya_contact_forms, save_and_upload
+from routes import  user_auth, referrals, admin, user_details, mortgage_applications, save_and_upload
 from routes.Reg import reg
 
 app = FastAPI()
@@ -11,7 +11,6 @@ app.include_router(admin.router)
 app.include_router(user_details.router)
 app.include_router(reg.router)
 app.include_router(mortgage_applications.router)
-app.include_router(anaya_contact_forms.router)
 app.include_router(save_and_upload.router)
 
 app.add_middleware(
